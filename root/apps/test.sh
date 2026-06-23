1ecd732806a5978bc90096407c65b71d04af45a9
Christopher Schnick <crschnick@xpipe.io>
2026-06-23 06:59:13 +0200

    [stage]

--- root/apps/test.sh
+++ root/apps/test.sh
@@ -11,7 +11,7 @@ sudo chgrp -R abc "$USER_HOME"
 cd "$HOME"
 
 if [ -z "$1" ]; then
-  exclude=("/apps/available/vscode.sh", "/apps/available/tailscale.sh", "/apps/available/netbird.sh")
+  exclude=("/apps/available/vscode.sh" "/apps/available/tailscale.sh" "/apps/available/netbird.sh")
   for file in /apps/available/*; do
       printf '%s\0' "${exclude[@]}" | grep -F -x -z -- "$file" && continue
       name=$(basename $file .sh)

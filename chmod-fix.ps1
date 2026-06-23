1282be1ae98de9b1000b911c916a5ed086b98323
Christopher Schnick <crschnick@xpipe.io>
2026-06-23 05:26:47 +0200

    Fix permissions

---
+++ chmod-fix.ps1
@@ -0,0 +1,10 @@
+$Targets = "root/apps", "root/defaults/autostart.sh", "root/defaults/desktop.sh", "root/defaults/mobile.sh", "root/defaults/startwm_wayland.sh", "root/defaults/xpipe_install.sh", "root/etc/s6-overlay/s6-rc.d/netbirdd/up", "root/etc/s6-overlay/s6-rc.d/packagekitd/run", "root/etc/s6-overlay/s6-rc.d/polkitd/run", "root/etc/s6-overlay/s6-rc.d/tailscaled/run"
+
+foreach ($Target in $Targets) {
+  Get-ChildItem -Path $Target -Recurse |
+    Where-Object { -not $_.PSIsContainer } |
+    ForEach-Object {
+      echo $_.FullName
+      git update-index --chmod=+x $_.FullName
+    }
+}

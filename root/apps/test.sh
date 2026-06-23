be544acddea0ef6feaa8d6e4dd996ea6b54f98b4
Christopher Schnick <crschnick@xpipe.io>
2026-06-23 06:46:17 +0200

    [stage]

--- root/apps/test.sh
+++ root/apps/test.sh
@@ -11,7 +11,9 @@ sudo chgrp -R abc "$USER_HOME"
 cd "$HOME"
 
 if [ -z "$1" ]; then
+  exclude=("/apps/available/aws.sh")
   for file in /apps/available/*; do
+      printf '%s\0' "${exclude[@]}" | grep -F -x -z -- "$file" && continue
       name=$(basename $file .sh)
       /apps/install.sh $name
   done

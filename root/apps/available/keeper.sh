c8fc8d1c1a7df942e523f52c8a5d1315f4e5c2f5
Christopher Schnick <crschnick@xpipe.io>
2026-07-29 20:37:56 +0200

    [stage]

--- root/apps/available/keeper.sh
+++ root/apps/available/keeper.sh
@@ -4,6 +4,7 @@ sudo apt install -y python3 python3-pip
 python3 -m venv keeper-env
 source keeper-env/bin/activate
 pip install keepercommander
-sudo mkdir -p /config/.local/bin
+mkdir -p /config/.local/bin
+mkdir -p /config/.local/share
 sudo ln -s /config/keeper-env/bin/keeper /config/.local/bin/keeper
 keeper --version
